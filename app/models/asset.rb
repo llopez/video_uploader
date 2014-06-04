474b00f5440f5847e51284eec5edf9f88d77fc3d
Luis Lopez <luigibyte@gmail.com>
2014-06-04 16:59:21 -0300

    setup paperclip to use s3 storage

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,7 +3,9 @@ class Asset < ActiveRecord::Base
                     styles: { 
                       medium: "300x300>", thumb: "100x100>" 
                     }, 
-                    default_url: "/images/:style/missing.png"
+                    default_url: "/images/:style/missing.png",
+                    storage: :s3,
+                    s3_credentials: Rails.configuration.s3_credentials
 
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 end
