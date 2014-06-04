dfa3f60bf9ae514966e9aa9067adb032b55fb69b
Luis Lopez <luigibyte@gmail.com>
2014-06-04 16:30:23 -0300

    default paperclip config for asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,2 +1,9 @@
 class Asset < ActiveRecord::Base
+  has_attached_file :attachment, 
+                    styles: { 
+                      medium: "300x300>", thumb: "100x100>" 
+                    }, 
+                    default_url: "/images/:style/missing.png"
+
+  validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 end
